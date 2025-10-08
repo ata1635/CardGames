e6dbd755d3a967b4041071f030c43a2c067dc6ac
Ata <115926805+ata1635@users.noreply.github.com>
2025-10-08 11:14:01 +0200

    test: Deck contains no duplicate cards

--- src/test/java/com/ata1635/cardgame/core/DeckTest.java
+++ src/test/java/com/ata1635/cardgame/core/DeckTest.java
@@ -1,9 +1,14 @@
 package com.ata1635.cardgame.core;
 
+import com.ata1635.cardgame.core.model.Card;
 import com.ata1635.cardgame.core.service.Deck;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,5 +27,16 @@ public class DeckTest {
     void testDeckHas52Cards() {
         assertThat(deck.getDeck()).hasSize(52);
     }
+
+    @Test
+    @DisplayName("Deck contains no duplicate cards")
+    void testAllCardsUnique() {
+        List<Card> cards = deck.getDeck();
+        Set<String> uniqueCards = cards.stream()
+                .map(Card::toString)
+                .collect(Collectors.toSet());
+        assertThat(uniqueCards).hasSize(52);
+    }
+
 
 }
