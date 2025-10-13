34ff75e77ed9619135c391348eaa1d00838fdfbf
Ata <115926805+ata1635@users.noreply.github.com>
2025-10-13 09:38:22 +0200

    feat: Add green game panel

--- src/main/java/com/ata1635/cardgame/core/games/BlackjackUI.java
+++ src/main/java/com/ata1635/cardgame/core/games/BlackjackUI.java
@@ -1,20 +1,28 @@
 package com.ata1635.cardgame.core.games;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class BlackjackUI extends JFrame {
 
     private Blackjack blackjack;
+    private JFrame frame;
+    private JPanel gamePanel;
 
     public BlackjackUI() {
         blackjack = new Blackjack();
+        frame = new JFrame("Black Jack");
+        gamePanel = new JPanel();
 
-        JFrame frame = new JFrame("Black Jack");
         frame.setVisible(true);
         frame.setSize(600, 600);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        gamePanel.setLayout(new BorderLayout());
+        gamePanel.setBackground(new Color(53, 101, 77));
+        frame.add(gamePanel);
 
     }
 }
