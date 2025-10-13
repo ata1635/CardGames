01060d3b46bdd5645f05d07f44f5ffa0cd820a2d
Ata <115926805+ata1635@users.noreply.github.com>
2025-10-13 08:34:51 +0200

    feat: game logic for calculating card values
    - add Blackjack class
    - build deck and shuffle it
    - dealer and player get cards dealt and the values are calculated
    - dealer gets a hidden card which will be revealed at the end of the game
    - getters and setters for testing the sums of cards etc. for testing

--- src/main/java/com/ata1635/cardgame/core/games/Blackjack.java
+++ src/main/java/com/ata1635/cardgame/core/games/Blackjack.java
@@ -9,26 +9,70 @@ import java.util.List;
 public class Blackjack {
 
     private Deck deck;
+    private Card hiddenCard;
+
+    private ArrayList<Card> dealerHand;
+    private int dealerSum;
+    private int dealerAceCount; //devalue the Ace from 11 to 1
+
+    private ArrayList<Card> playerHand;
+    private int playerSum;
+    private int playerAceCount;
 
     public Blackjack() {
-        deck = new Deck();
-        deck.shuffleDeck();
+        this.deck = new Deck();
+        this.deck.shuffleDeck();
+        startGame();
+    }
 
+    //constructor for testing
+    public Blackjack(Deck custonDeck) {
+        this.deck = custonDeck;
         startGame();
     }
 
     public void startGame() {
+        ArrayList<Card> deck = new ArrayList<>(this.deck.getDeck());
 
         //dealer
-        Card hiddenCard;
-        ArrayList<Card> dealerHand;
-        int dealerSum;
-        int dealerAceCount; //devalue the Ace from 11 to 1
+        dealerHand = new ArrayList<Card>();
+        dealerSum = 0;
+        dealerAceCount = 0;
+
+        hiddenCard = deck.remove(deck.size() - 1);
+        dealerSum += hiddenCard.getValue();
+        dealerAceCount += hiddenCard.isAce() ? 1 : 0;
+
+        Card card = deck.remove(deck.size() - 1); //dealer only draws one card
+        dealerSum += card.getValue();
+        dealerAceCount += card.isAce() ? 1 : 0;
+        dealerHand.add(card);
+
+        System.out.println("DEALER:\n" + hiddenCard + "\n"
+                + dealerHand + "\n" + dealerSum + "\n" + dealerAceCount + "\n");
 
         //player
-        ArrayList<Card> playerHand;
-        int playerSum;
-        int playerAceCount;
+        playerHand = new ArrayList<Card>();
+        playerSum = 0;
+        playerAceCount = 0;
 
+        for (int i = 0; i < 2; i++) { //player draws 2 cards at start
+            card = deck.remove(deck.size() - 1);
+            playerSum += card.getValue();
+            playerAceCount += card.isAce() ? 1 : 0;
+            playerHand.add(card);
+        }
+
+        System.out.println("PLAYER:\n" + playerHand + "\n" + playerSum + "\n" + playerAceCount);
     }
+
+    //getters for testing
+    public Card getHiddenCard() { return hiddenCard; }
+    public List<Card> getDealerHand() { return dealerHand; }
+    public List<Card> getPlayerHand() { return playerHand; }
+    public int getDealerSum() { return dealerSum; }
+    public int getPlayerSum() { return playerSum; }
+    public int getDealerAceCount() { return dealerAceCount; }
+    public int getPlayerAceCount() { return playerAceCount; }
+
 }
