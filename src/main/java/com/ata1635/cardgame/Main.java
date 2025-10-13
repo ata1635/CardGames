f4cc00028d109e4a3869d1a436adbdd56ef30388
Ata <115926805+ata1635@users.noreply.github.com>
2025-10-13 09:28:17 +0200

    chore: Change how the game starts
    - the game now stars from the BlackjackUI instead of the Main class

--- src/main/java/com/ata1635/cardgame/Main.java
+++ src/main/java/com/ata1635/cardgame/Main.java
@@ -1,9 +1,12 @@
 package com.ata1635.cardgame;
 
 import com.ata1635.cardgame.core.games.Blackjack;
+import com.ata1635.cardgame.core.games.BlackjackUI;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Blackjack blackJack = new Blackjack();
+        SwingUtilities.invokeLater(() -> new BlackjackUI());
     }
 }
